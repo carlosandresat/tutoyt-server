922dbc5dd6f502e8b874ef8ce2bfe2077739e817
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2023-04-19 00:12:00 -0500

    login authentication with jwt

--- src/controllers/session.controllers.js
+++ src/controllers/session.controllers.js
@@ -1,17 +1,17 @@
 import { pool } from "../db.js"
 
 export const requestSession = async (req, res) => {
-    const { student, tutor, classname, date, time} = req.body;
+    const { student, tutor, className, date, time} = req.body;
     const [result] = await pool.query(
         "INSERT INTO session(id_student, id_tutor, id_class, status, date, time) VALUES (?, (SELECT id FROM user where name= ?), (SELECT id FROM classes where name= ?), 'requested', ?, ?)",
-        [student, tutor, classname, date, time]
+        [student, tutor, className, date, time]
     );
     console.log(result);
     res.json({
         id: result.insertId,
         student, 
         tutor, 
-        classname, 
+        className, 
         date, 
         time
     });
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 import {pool} from './db.js'
 import {PORT} from './config.js'
 
@@ -8,6 +10,7 @@ import indexRoutes from './routes/index.routes.js'
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors())
 
 app.use(indexRoutes)
@@ -23,6 +26,23 @@ app.get('/tutores', async (req, res)=>{
 
     const [rows] = await pool.query('SELECT t2.id_tutor AS id, t1.name, TRUNCATE(t2.rating, 1) AS rating, t2.nreviews FROM user t1 INNER JOIN (SELECT id_tutor, AVG(stars) AS rating, COUNT(stars) as nreviews FROM session GROUP BY id_tutor) t2 WHERE t1.id = t2.id_tutor')
     res.json(rows)    
+})
+
+app.post('/login', async (req, res)=>{
+    const { user, password } = req.body;
+    const [result] = await pool.query(
+        'SELECT * FROM user WHERE user = ? AND password = ?',
+        [user, password]
+    )
+    if(result.length > 0) {
+        const user = result[0].user
+        const token = jwt.sign({user}, "tutoyt-key0963963170", {expiresIn: '1d'})
+        res.cookie('token', token)
+        return res.json({Status: "Success"})
+    }
+    else{
+        return res.json({message: "Usuario o contraseña no válidos"})
+    }
 })
 
 // Server running -------------------
