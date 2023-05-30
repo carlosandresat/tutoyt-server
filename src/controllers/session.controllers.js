9572ca46a48cab9404271db18bf549271c30676c
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2023-05-30 10:37:11 -0500

    Update updateSession info to handle status to 'changed'

--- src/controllers/session.controllers.js
+++ src/controllers/session.controllers.js
@@ -22,7 +22,7 @@ export const requestSession = async (req, res) => {
 }
 
 export const getSessionsByTutor = async (req, res) => {
-    const [result] = await pool.query("SELECT session.id, session.topic, session.status, date as date_raw, DATE_FORMAT(date, '%d/%c/%Y') AS date, TIME_FORMAT(time, '%H:%i') as time, classes.name as classname, user.name as student FROM session INNER JOIN classes ON session.id_class = classes.id INNER JOIN user ON session.id_student = user.id WHERE id_tutor = (SELECT id from user WHERE user = ?) AND date >= CURDATE() ORDER BY date_raw, time", [
+    const [result] = await pool.query("SELECT session.id, session.topic, session.status, session.place, date as date_raw, DATE_FORMAT(date, '%d/%c/%Y') AS date, TIME_FORMAT(time, '%H:%i') as time, classes.name as classname, user.name as student FROM session INNER JOIN classes ON session.id_class = classes.id INNER JOIN user ON session.id_student = user.id WHERE id_tutor = (SELECT id from user WHERE user = ?) AND date >= CURDATE() ORDER BY date_raw, time", [
         req.params.user,
     ])
     if(result.length == 0)
@@ -32,7 +32,7 @@ export const getSessionsByTutor = async (req, res) => {
 }
 
 export const getSessionsByStudent = async (req, res) => {
-    const [result] = await pool.query("SELECT session.id, status, date as date_raw, DATE_FORMAT(date, '%d/%c/%Y') AS date, TIME_FORMAT(time, '%H:%i') as time, name, code FROM session INNER JOIN (SELECT classes.id, classes.name, code FROM classes INNER JOIN school ON classes.id_school = school.id) classdata ON session.id_class = classdata.id WHERE id_student = (SELECT id from user WHERE user = ?) AND date >= CURDATE() ORDER BY date_raw, time", [
+    const [result] = await pool.query("SELECT session.id, status, date as date_raw, DATE_FORMAT(date, '%d/%c/%Y') AS date, TIME_FORMAT(time, '%H:%i') as time, session.place, name, code FROM session INNER JOIN (SELECT classes.id, classes.name, code FROM classes INNER JOIN school ON classes.id_school = school.id) classdata ON session.id_class = classdata.id WHERE id_student = (SELECT id from user WHERE user = ?) AND date >= CURDATE() ORDER BY date_raw, time", [
         req.params.user,
     ])
     if(result.length == 0)
@@ -58,7 +58,7 @@ export const acceptSession = async (req, res) => {
 export const updateDate = async (req, res) => {
     const { date, time } = req.body;
 
-    const result = await pool.query("UPDATE session SET date = ?, time = ?, changes = 'fecha/hora' WHERE id = ?", [
+    const result = await pool.query("UPDATE session SET date = ?, time = ?, changes = 'fecha/hora', status = 'changed' WHERE id = ?", [
         date, time, req.params.sessionId
     ]);
     res.json({ 
@@ -70,7 +70,7 @@ export const updateDate = async (req, res) => {
 export const updatePlace = async (req, res) => {
     const { place } = req.body;
 
-    const result = await pool.query("UPDATE session SET place = ?, changes = 'lugar' WHERE id = ?", [
+    const result = await pool.query("UPDATE session SET place = ?, changes = 'lugar', status = 'changed' WHERE id = ?", [
         place, req.params.sessionId
     ]);
     res.json({ place })
@@ -79,7 +79,7 @@ export const updatePlace = async (req, res) => {
 export const updateTopic = async (req, res) => {
     const { topic } = req.body;
 
-    const result = await pool.query("UPDATE session SET topic = ?, changes = 'tema' WHERE id = ?", [
+    const result = await pool.query("UPDATE session SET topic = ?, changes = 'tema', status = 'changed' WHERE id = ?", [
         topic, req.params.sessionId
     ]);
     res.json({ topic })
@@ -88,7 +88,7 @@ export const updateTopic = async (req, res) => {
 export const updateDatePlace = async (req, res) => {
     const { date, time, place } = req.body;
 
-    const result = await pool.query("UPDATE session SET date = ?, time = ?,  place = ?, changes = 'fecha/hora y lugar' WHERE id = ?", [
+    const result = await pool.query("UPDATE session SET date = ?, time = ?,  place = ?, changes = 'fecha/hora y lugar', status = 'changed' WHERE id = ?", [
         date, time, place, req.params.sessionId
     ]);
     res.json({ date, time, place })
@@ -97,7 +97,7 @@ export const updateDatePlace = async (req, res) => {
 export const updateDateTopic = async (req, res) => {
     const { date, time, topic } = req.body;
 
-    const result = await pool.query("UPDATE session SET date = ?, time = ?,  topic = ?, changes = 'fecha/hora y tema' WHERE id = ?", [
+    const result = await pool.query("UPDATE session SET date = ?, time = ?,  topic = ?, changes = 'fecha/hora y tema', status = 'changed' WHERE id = ?", [
         date, time, topic, req.params.sessionId
     ]);
     res.json({ date, time, topic })
@@ -106,7 +106,7 @@ export const updateDateTopic = async (req, res) => {
 export const updatePlaceTopic = async (req, res) => {
     const { place, topic } = req.body;
 
-    const result = await pool.query("UPDATE session SET place = ?,  topic = ?, changes = 'lugar y tema' WHERE id = ?", [
+    const result = await pool.query("UPDATE session SET place = ?,  topic = ?, changes = 'lugar y tema', status = 'changed' WHERE id = ?", [
         place, topic, req.params.sessionId
     ]);
     res.json({ place, topic })
@@ -115,7 +115,7 @@ export const updatePlaceTopic = async (req, res) => {
 export const updateAll = async (req, res) => {
     const { date, time, place, topic } = req.body;
 
-    const result = await pool.query("UPDATE session SET date = ?, time = ?, place = ?,  topic = ?, changes = 'fecha/hora, lugar y tema' WHERE id = ?", [
+    const result = await pool.query("UPDATE session SET date = ?, time = ?, place = ?,  topic = ?, changes = 'fecha/hora, lugar y tema', status = 'changed' WHERE id = ?", [
         date, time, place, topic, req.params.sessionId
     ]);
     res.json({
